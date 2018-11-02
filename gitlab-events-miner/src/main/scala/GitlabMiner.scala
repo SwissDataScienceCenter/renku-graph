fa5dc36f2ff5d8c3ce2eea5d75a69e453ffad376
3C111 <digital_urn@hotmail.com>
2018-11-02 11:17:18 +0100

    Added case class for easier parsing

--- gitlab-events-miner/src/main/scala/GitlabMiner.scala
+++ gitlab-events-miner/src/main/scala/GitlabMiner.scala
@@ -3,9 +3,9 @@ This module reads the events table from the gitlab instance and publishes the pu
  */
 package GitlabMiner
 
+import GitlabMiner.helperFunctions.Event
 import java.sql.{DriverManager, ResultSet}
 import scala.collection.mutable.ListBuffer
-
 import com.typesafe.config.{Config, ConfigFactory}
 
 
@@ -21,20 +21,21 @@ object Main extends App {
 
 
   // Continuous poll
-  val allevents = new ListBuffer[List[(String, String)]]()
+  val allevents = new ListBuffer[Event]()
 
   for (i <- 1 to 3) {
     //while (1==1) {
-    val id =  if (allevents.isEmpty){3} else { allevents.tail.last.head._2}
+    val id =  if (allevents.isEmpty){1} else { allevents.last.id }
     val prep = conn.prepareStatement(s"select * from events as e, push_event_payloads as p where p.event_id = e.id and e.id >$id;")
     val result = prep.executeQuery
     while ( result.next() ) {
-      val t = for (c <- helperFunctions.all_columns_list) yield (c.toString, result.getString(c))
+      val t = (Event.apply _).tupled(helperFunctions.getresult(result) )
+
+      println(t)
       //send t to wherever
       allevents += t
 
     }
-    println(allevents)
   }
   conn.close()
 
--- gitlab-events-miner/src/main/scala/helperFunctions.scala
+++ gitlab-events-miner/src/main/scala/helperFunctions.scala
@@ -1,7 +1,6 @@
 package GitlabMiner
 
 import java.sql.ResultSet
-import scala.collection.mutable.ListBuffer
 
 object helperFunctions {
 
@@ -21,5 +20,34 @@ object helperFunctions {
   val events_columns_list = List("id", "project_id", "author_id", "target_id",  "created_at",  "updated_at", "action",  "target_type")
   val push_payload_columns_list = List( "commit_count", "event_id", "action", "ref_type", "commit_from", "commit_to", "ref", "commit_title")
   val all_columns_list = events_columns_list:::push_payload_columns_list
+
+
+  def getresult(result: ResultSet) = {
+      (result.getInt("id"), result.getInt( "project_id"), result.getInt("author_id"), result.getInt("target_id"),
+      result.getTimestamp("created_at"), result.getTimestamp("updated_at"), result.getInt("action"),
+      result.getString("target_type"), result.getInt("commit_count"), result.getInt("event_id"),
+      result.getInt("action"), result.getInt("ref_type"), result.getBytes("commit_from"),
+      result.getBytes("commit_to"), result.getString("ref"), result.getString("commit_title")   )
+  }
+
+
+  //TODO is target_id needed also is it int?
+
+  case class Event(id: Int, project_id: Int, author_id: Int, target_id: Int,
+                   created_at: java.sql.Timestamp, updated_at: java.sql.Timestamp, actionE: Int, target_type: String,
+                   commit_count: Int, event_id: Int, actionP: Int, ref_type: Int, commit_from:  Array[Byte],
+                   commit_to : Array[Byte], ref: String, commit_title: String)
+  object Event extends Function16[Int, Int, Int, Int, java.sql.Timestamp, java.sql.Timestamp, Int, String, Int, Int, Int, Int, Array[Byte], Array[Byte], String, String, Event]
+
+  case class PushEvent(id: Int, project_id: Int, author_id: Int,
+                       created_at:  java.sql.Timestamp, updated_at:  java.sql.Timestamp, commit_count: Int,
+                       action: Int, ref_type: Int, commit_from:  Array[Byte],
+                       commit_to : Array[Byte], ref: String, commit_title: String)
+
+  case class SingleCommit(id: Int, short_id: Int, title: String, author_name: String,
+                          author_email: String, committer_name: String, committer_email: String,
+                          created_at:  java.sql.Timestamp, message: String, committed_date:  java.sql.Timestamp,
+                          authored_date: java.sql.Timestamp, parent_ids: List[Int]
+                         )
 
 }
