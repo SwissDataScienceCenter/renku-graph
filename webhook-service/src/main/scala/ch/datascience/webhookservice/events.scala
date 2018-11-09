a517a240bb26c0a53bc9c5ac902b10d9381c4cc4
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2018-11-09 10:56:15 +0100

    #7 Improved PushEvent modeling

--- webhook-service/src/main/scala/ch/datascience/webhookservice/events.scala
+++ webhook-service/src/main/scala/ch/datascience/webhookservice/events.scala
@@ -1,17 +1,13 @@
 package ch.datascience.webhookservice
 
+import ch.datascience.tinytypes.TinyType
+
 case class PushEvent(before: CommitBefore,
                      after: CommitAfter,
                      projectId: ProjectId)
 
-case class CommitBefore(value: String) {
-  override val toString: String = value
-}
+case class CommitBefore(value: String) extends GitSha
 
-case class CommitAfter(value: String) {
-  override val toString: String = value
-}
+case class CommitAfter(value: String) extends GitSha
 
-case class ProjectId(value: Long) {
-  override val toString: String = value.toString
-}
+case class ProjectId(value: Long) extends TinyType[Long]
