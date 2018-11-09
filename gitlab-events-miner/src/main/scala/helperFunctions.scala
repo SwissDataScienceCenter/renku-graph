5f20ab2dc456b2e2aa8b0f11e5e91a3d6ab52804
3C111 <digital_urn@hotmail.com>
2018-11-09 17:07:01 +0100

    Protection from non-existing projects added

--- gitlab-events-miner/src/main/scala/helperFunctions.scala
+++ gitlab-events-miner/src/main/scala/helperFunctions.scala
@@ -12,19 +12,18 @@ object helperFunctions {
 
 
   def getresult(result: ResultSet) = {
-      (result.getInt("id"), result.getInt( "project_id"), result.getInt("author_id"), result.getInt("target_id"),
+    (result.getInt("id"), result.getInt( "project_id"), result.getInt("author_id"), result.getInt("target_id"),
       result.getTimestamp("created_at"), result.getTimestamp("updated_at"), result.getInt("action"),
       result.getString("target_type"), result.getInt("commit_count"), result.getInt("event_id"),
-      result.getInt("action"), result.getInt("ref_type"), result.getBytes("commit_from"),
-      result.getBytes("commit_to"), result.getString("ref"), result.getString("commit_title"))
+      result.getInt("action"), result.getInt("ref_type"), Option(result.getBytes("commit_from")),
+      Option(result.getBytes("commit_to")), result.getString("ref"), result.getString("commit_title")   )
   }
 
   case class Event(id: Int, project_id: Int, author_id: Int, target_id: Int,
                    created_at: java.sql.Timestamp, updated_at: java.sql.Timestamp, actionE: Int, target_type: String,
-                   commit_count: Int, event_id: Int, actionP: Int, ref_type: Int, commit_from: Array[Byte],
-                   commit_to : Array[Byte], ref: String, commit_title: String)
-  object Event extends Function16[Int, Int, Int, Int, java.sql.Timestamp, java.sql.Timestamp, Int, String, Int, Int, Int, Int, Array[Byte], Array[Byte], String, String, Event]
-
+                   commit_count: Int, event_id: Int, actionP: Int, ref_type: Int, commit_from: Option[Array[Byte]],
+                   commit_to : Option[Array[Byte]], ref: String, commit_title: String)
+  object Event extends Function16[Int, Int, Int, Int, java.sql.Timestamp, java.sql.Timestamp, Int, String, Int, Int, Int, Int, Option[Array[Byte]], Option[Array[Byte]], String, String, Event]
   case class GitSingleCommit(id: String, short_id: String, title: String, author_name: String,
                              author_email: String, committer_name: String, committer_email: String,
                              created_at: String, message: String, committed_date: String,
