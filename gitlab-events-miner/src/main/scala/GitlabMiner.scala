c1aaa43e012b3d1d7bfb3b418ecd249e0f46ed68
3C111 <digital_urn@hotmail.com>
2018-11-09 11:50:51 +0100

    Added functions to get all single commits, does not yet work for merge requests

--- gitlab-events-miner/src/main/scala/GitlabMiner.scala
+++ gitlab-events-miner/src/main/scala/GitlabMiner.scala
@@ -3,12 +3,14 @@ This module reads the events table from the gitlab instance and publishes the pu
  */
 package GitlabMiner
 
-import GitlabMiner.helperFunctions.Event
+import GitlabMiner.helperFunctions.{Event, GitSingleCommit, SingleCommit}
 import java.sql.{DriverManager, ResultSet}
+import spray.json.DefaultJsonProtocol._
+import spray.json.RootJsonFormat
 import scala.collection.mutable.ListBuffer
 import com.typesafe.config.{Config, ConfigFactory}
-
-
+import scalaj.http._
+import spray.json._
 
 object Main extends App {
 
@@ -25,18 +27,54 @@ object Main extends App {
 
   for (i <- 1 to 3) {
     //while (1==1) {
-    val id =  if (allevents.isEmpty){1} else { allevents.last.id }
+    val id =  if (allevents.isEmpty){150} else { allevents.last.id }
     val prep = conn.prepareStatement(s"select * from events as e, push_event_payloads as p where p.event_id = e.id and e.id >$id;")
     val result = prep.executeQuery
     while ( result.next() ) {
       val t = (Event.apply _).tupled(helperFunctions.getresult(result) )
+      // get the amount of commits: start from commit_to until commit_from loop over parents
+      // count how we many commits we go back, in case of merge req
 
-      println(t)
-      //send t to wherever
+      //GET /projects/:id/repository/commits/:sha
+      val response: HttpResponse[String] = Http("https://testing.datascience.ch/api/v4/projects/22/repository/commits/b8bc9bf560921c6dc027529393c57134b5619b44").asString
+      val commit_count = t.commit_count
+
+      //if commit_count = 1 get
+    /*  val response: HttpResponse[Map[String,String]] = Http("https://testing.datascience.ch/api/v4/projects/22/repository/commits/b8bc9bf560921c6dc027529393c57134b5619b44").execute(parser = {inputStream =>
+        Json.parse[Map[String,String]](inputStream)
+      })*/
+
+      print((response.body).parseJson)
       allevents += t
 
     }
   }
+
+  def getAllCommits(project_id: Int, commit_to : String, commit_from: String, commit_count: Int, singlecommit: GitSingleCommit, count: Int, list: List[GitSingleCommit]): List[GitSingleCommit] = {
+    println(commit_count)
+
+    if (commit_count == 1 ||  commit_count == count ||
+      (singlecommit.parent_ids.length == 1 && (commit_from == singlecommit.parent_ids.head)))  {
+      list:+singlecommit
+    }
+
+    else {
+      getCommit(project_id, singlecommit.parent_ids.head, commit_from, commit_count, count+1, list:+singlecommit)
+    }
+  }
+
+  def getCommit(project_id: Int, commit_to : String, commit_from: String, commit_count: Int, count: Int, list: List[GitSingleCommit]): List[GitSingleCommit] = {
+    // print(commit_to)
+    val response: HttpResponse[String] =
+      Http("https://testing.datascience.ch/api/v4/projects/"+ project_id + "/repository/commits/" +
+        commit_to).asString
+    val firstCommit = ((response.body).parseJson).convertTo[GitSingleCommit]
+    return getAllCommits(project_id, commit_to : String, commit_from: String, commit_count: Int, firstCommit, count, list: List[GitSingleCommit])
+
+  }
+
+  implicit val gitsingleCommitFormat: RootJsonFormat[GitSingleCommit] = jsonFormat12(GitSingleCommit.apply)
+
   conn.close()
 
 }
