0411b41f427818d91c9518e7da0825f8c509e67d
Rok Roskar <rok.roskar@sdsc.ethz.ch>
2019-08-06 13:13:31 +0200

    build: push charts on merges to master

--- .travis/travis-release.sh
+++ .travis/travis-release.sh
@@ -34,4 +34,20 @@ if [[ -z $TRAVIS_TAG ]] && [[ ! $TRAVIS_COMMIT_MESSAGE =~ $COMMIT_MESSAGE_PATTER
 
   # releasing graph-services
   sbt "release skip-tests default-tag-exists-answer k  with-defaults"
+
+  # push the charts and images
+  make login
+
+  # decrypt ssh key to use for docker hub login
+  openssl aes-256-cbc -K $encrypted_b7eb5d86688a_key -iv $encrypted_b7eb5d86688a_iv -in deploy_rsa.enc -out deploy_rsa -d
+  chmod 600 deploy_rsa
+  eval $(ssh-agent -s)
+  ssh-add deploy_rsa
+
+  # build charts/images and push
+  cd helm-chart
+  chartpress --push --publish-chart
+  git diff
+  # push also images tagged with "latest"
+  chartpress --tag latest --push
 fi
