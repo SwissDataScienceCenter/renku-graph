2f52416a18ff2de06c92e27e7d8e1c0c7db42700
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2020-05-04 09:57:16 +0200

    fix: CLEAR DEFAULT as the all triples removal query

--- triples-generator/src/main/scala/ch/datascience/triplesgenerator/reprovisioning/TriplesRemover.scala
+++ triples-generator/src/main/scala/ch/datascience/triplesgenerator/reprovisioning/TriplesRemover.scala
@@ -43,7 +43,7 @@ private class IOTriplesRemover(
     SparqlQuery(
       name     = "all triples remove",
       prefixes = Set.empty,
-      body     = "DROP DEFAULT"
+      body     = "CLEAR DEFAULT"
     )
   }
 }
