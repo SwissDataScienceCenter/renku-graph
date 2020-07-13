7a772d19bfc1c890392fc15afbba0a8a7c8272c8
David Kunzmann <joke1196@users.noreply.github.com>
2020-07-13 11:42:26 +0200

    fix: added logs when git clone commands fails (#340)
    * fix: added logs when git clone commands fails
    
    Co-authored-by: Andreas Bleuler <ableuler@users.noreply.github.com>
    Co-authored-by: RenkuBot <renku@datascience.ch>

--- triples-generator/src/main/scala/ch/datascience/triplesgenerator/eventprocessing/triplesgeneration/renkulog/RenkuLogTriplesGenerator.scala
+++ triples-generator/src/main/scala/ch/datascience/triplesgenerator/eventprocessing/triplesgeneration/renkulog/RenkuLogTriplesGenerator.scala
@@ -20,7 +20,6 @@ package ch.datascience.triplesgenerator.eventprocessing.triplesgeneration.renkul
 
 import java.security.SecureRandom
 
-import Commands.GitLabRepoUrlFinder
 import cats.data.EitherT
 import cats.data.EitherT.right
 import cats.effect.{ContextShift, IO, Timer}
@@ -34,6 +33,8 @@ import ch.datascience.triplesgenerator.eventprocessing.CommitEvent._
 import ch.datascience.triplesgenerator.eventprocessing.CommitEventProcessor.ProcessingRecoverableError
 import ch.datascience.triplesgenerator.eventprocessing.triplesgeneration.TriplesGenerator
 import ch.datascience.triplesgenerator.eventprocessing.triplesgeneration.TriplesGenerator.GenerationRecoverableError
+import ch.datascience.triplesgenerator.eventprocessing.triplesgeneration.renkulog.Commands.GitLabRepoUrlFinder
+import io.chrisdavenport.log4cats.Logger
 
 import scala.concurrent.ExecutionContext
 import scala.language.{higherKinds, postfixOps}
@@ -119,9 +120,11 @@ private[eventprocessing] class RenkuLogTriplesGenerator private[renkulog] (
 
 private[eventprocessing] object RenkuLogTriplesGenerator {
 
-  def apply()(implicit contextShift: ContextShift[IO],
-              executionContext:      ExecutionContext,
-              timer:                 Timer[IO]): IO[TriplesGenerator[IO]] =
+  def apply()(
+      implicit contextShift: ContextShift[IO],
+      executionContext:      ExecutionContext,
+      timer:                 Timer[IO]
+  ): IO[TriplesGenerator[IO]] =
     for {
       renkuLogTimeout <- RenkuLogTimeout[IO]()
       gitLabUrl       <- GitLabUrl[IO]()
