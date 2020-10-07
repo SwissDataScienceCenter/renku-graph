f1196db5dcc15daea283e8d1b4e8348a13866d00
David Kunzmann <davidkunzmann@gmail.com>
2020-10-07 12:17:26 +0200

    fix: making a proper loop for gauge reset

--- event-log/src/main/scala/io/renku/eventlog/metrics/EventGaugeScheduler.scala
+++ event-log/src/main/scala/io/renku/eventlog/metrics/EventGaugeScheduler.scala
@@ -6,6 +6,8 @@ import cats.syntax.all._
 import ch.datascience.metrics.LabeledGauge
 import io.chrisdavenport.log4cats.Logger
 
+import scala.concurrent.duration.FiniteDuration
+
 trait EventGaugeScheduler[Interpretation[_]] {
   def run(): Interpretation[Unit]
 }
@@ -18,9 +20,14 @@ class EventGaugeSchedulerImpl[Interpretation[_], LabelValue](
     extends EventGaugeScheduler[Interpretation] {
   override def run(): Interpretation[Unit] = (for {
     interval <- metricsSchedulerConfig.getInterval()
-    _        <- timer sleep interval
-    _        <- gauges.map(_.reset()).sequence
+    _        <- resetGaugesEvery(interval)
   } yield ()) recoverWith logError
+
+  private def resetGaugesEvery(interval: FiniteDuration): Interpretation[Unit] = for {
+    _ <- timer sleep interval
+    _ <- gauges.map(_.reset()).sequence
+    _ <- resetGaugesEvery(interval)
+  } yield ()
 
   private lazy val logError: PartialFunction[Throwable, Interpretation[Unit]] = { case e: Throwable =>
     logger.error(s"Clearing event gauge metrics failed with - ${e.getMessage}")
