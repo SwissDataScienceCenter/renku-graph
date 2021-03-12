1234d5113b7b1165dda38e784ffb428f27bb08d5
Travis Lee <travis.lee@sdsc.ethz.ch>
2021-03-12 11:15:56 +0100

    feat: new COMMIT_SYNC category

--- graph-commons/src/test/scala/ch/datascience/generators/Generators.scala
+++ graph-commons/src/test/scala/ch/datascience/generators/Generators.scala
@@ -359,8 +359,7 @@ object Generators {
       def toGeneratorOfOptions: Gen[Option[T]] = Gen.option(generator)
       def toGeneratorOfNonEmptyList(minElements: Int Refined Positive = 1,
                                     maxElements: Int Refined Positive = 5
-      ): Gen[NonEmptyList[T]] =
-        nonEmptyList(generator, minElements, maxElements)
+      ): Gen[NonEmptyList[T]] = nonEmptyList(generator, minElements, maxElements)
 
       def toGeneratorOfSet(minElements: Int Refined NonNegative = 1,
                            maxElements: Int Refined Positive = 5
