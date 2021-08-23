bc5c86e976317da9f1ea118e1424067ea65fea93
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2021-08-23 11:29:43 +0200

    Revert "test: use renku python from branch"
    This reverts commit 3dc70e6c92b8ecd67bc1d6b7e4eb82318846ce01.

--- triples-generator/entrypoint.sh
+++ triples-generator/entrypoint.sh
@@ -3,7 +3,7 @@
 if [ ! -z "$RENKU_PYTHON_DEV_VERSION" ]
 then
     /usr/bin/python3 -m pip uninstall --yes renku
-    /usr/bin/python3 -m pip install git+https://github.com/SwissDataScienceCenter/renku-python.git@2271-renku-graph-export
+    /usr/bin/python3 -m pip install ${RENKU_PYTHON_DEV_VERSION}
 fi
 
 # run the command
