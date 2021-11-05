efb52a269962f11cf9f05a6fb5fc75e55d94ff75
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2021-11-05 16:39:02 +0100

    feat: GET /projects/:id not to return parent if user has no access to it

--- knowledge-graph/src/test/scala/io/renku/knowledgegraph/projects/rest/KGProjectFinderSpec.scala
+++ knowledge-graph/src/test/scala/io/renku/knowledgegraph/projects/rest/KGProjectFinderSpec.scala
@@ -19,6 +19,8 @@
 package io.renku.knowledgegraph.projects.rest
 
 import cats.effect.IO
+import cats.syntax.all._
+import io.renku.generators.CommonGraphGenerators.authUsers
 import io.renku.generators.Generators.Implicits._
 import io.renku.graph.model.GraphModelGenerators._
 import io.renku.graph.model.testentities._
@@ -45,26 +47,63 @@ class KGProjectFinderSpec
       forAll(projectEntities(anyVisibility)) { project =>
         loadToStore(anyProjectEntities.generateOne, project)
 
-        metadataFinder.findProject(project.path).unsafeRunSync() shouldBe Some(project.to[KGProject])
+        metadataFinder.findProject(project.path, authUsers.generateOption).unsafeRunSync() shouldBe
+          project.to[KGProject].some
       }
     }
 
-    "return details of the project with the given path if it has a parent project" in new TestCase {
-      forAll(projectWithParentEntities(anyVisibility)) { project =>
+    "return details of the project with the given path if it has a parent project - public projects" in new TestCase {
+      forAll(projectWithParentEntities(visibilityPublic)) { project =>
         loadToStore(project, project.parent)
 
-        metadataFinder.findProject(project.path).unsafeRunSync() shouldBe Some(project.to[KGProject])
+        metadataFinder.findProject(project.path, authUsers.generateOption).unsafeRunSync() shouldBe
+          project.to[KGProject].some
       }
     }
 
+    "return details of the project with the given path if it has a parent project - non-public projects" in new TestCase {
+      val user         = authUsers.generateOne
+      val userAsMember = personEntities.generateOne.copy(maybeGitLabId = user.id.some)
+      val project      = projectWithParentEntities(visibilityNonPublic).generateOne.copy(members = Set(userAsMember))
+
+      val parent: Project = project.parent match {
+        case p: ProjectWithParent    => p.copy(members = Set(userAsMember))
+        case p: ProjectWithoutParent => p.copy(members = Set(userAsMember))
+      }
+
+      loadToStore(project, parent)
+
+      metadataFinder.findProject(project.path, user.some).unsafeRunSync() shouldBe
+        project.to[KGProject].some
+    }
+
+    "return details of the project with the given path without info about the parent " +
+      "if the user has no rights to access the parent project" in new TestCase {
+        val user = authUsers.generateOne
+        val project = projectWithParentEntities(visibilityNonPublic).generateOne.copy(
+          members = Set(personEntities.generateOne.copy(maybeGitLabId = user.id.some))
+        )
+
+        val parent: Project = project.parent match {
+          case p: ProjectWithParent    => p.copy(members = Set.empty)
+          case p: ProjectWithoutParent => p.copy(members = Set.empty)
+        }
+
+        loadToStore(project, parent)
+
+        metadataFinder.findProject(project.path, Some(user)).unsafeRunSync() shouldBe Some {
+          project.to[KGProject].copy(maybeParent = None)
+        }
+      }
+
     "return None if there's no project with the given path" in new TestCase {
-      metadataFinder.findProject(projectPaths.generateOne).unsafeRunSync() shouldBe None
+      metadataFinder.findProject(projectPaths.generateOne, authUsers.generateOption).unsafeRunSync() shouldBe None
     }
   }
 
   private trait TestCase {
     private implicit val logger: TestLogger[IO] = TestLogger[IO]()
     private val timeRecorder = new SparqlQueryTimeRecorder[IO](TestExecutionTimeRecorder[IO]())
-    val metadataFinder       = new KGProjectFinderImpl[IO](rdfStoreConfig, renkuBaseUrl, timeRecorder)
+    val metadataFinder       = new KGProjectFinderImpl[IO](rdfStoreConfig, timeRecorder)
   }
 }
