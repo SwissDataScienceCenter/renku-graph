a91d6a998cb7b967381943fcbed36a36e37bf12b
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2021-11-08 16:31:34 +0100

    feat: DatasetIdRecordsFinder

--- graph-commons/src/main/scala/io/renku/graph/http/server/security/DatasetIdRecordsFinder.scala
+++ graph-commons/src/main/scala/io/renku/graph/http/server/security/DatasetIdRecordsFinder.scala
@@ -22,40 +22,41 @@ import cats.effect.Async
 import cats.syntax.all._
 import io.circe.{Decoder, DecodingFailure}
 import io.renku.graph.http.server.security.Authorizer.{SecurityRecord, SecurityRecordFinder}
-import io.renku.graph.model.projects
 import io.renku.graph.model.projects.Visibility
-import io.renku.graph.model.projects.Visibility._
 import io.renku.graph.model.users.GitLabId
+import io.renku.graph.model.{datasets, projects}
 import io.renku.rdfstore.SparqlQuery.Prefixes
 import io.renku.rdfstore._
 import org.typelevel.log4cats.Logger
 
-object ProjectPathRecordsFinder {
-  def apply[F[_]: Async: Logger](timeRecorder: SparqlQueryTimeRecorder[F]): F[SecurityRecordFinder[F, projects.Path]] =
-    for {
-      config <- RdfStoreConfig[F]()
-    } yield new ProjectPathRecordsFinderImpl(config, timeRecorder)
+object DatasetIdRecordsFinder {
+  def apply[F[_]: Async: Logger](
+      timeRecorder: SparqlQueryTimeRecorder[F]
+  ): F[SecurityRecordFinder[F, datasets.Identifier]] = for {
+    config <- RdfStoreConfig[F]()
+  } yield new DatasetIdRecordsFinderImpl(config, timeRecorder)
 }
 
-private class ProjectPathRecordsFinderImpl[F[_]: Async: Logger](
+private class DatasetIdRecordsFinderImpl[F[_]: Async: Logger](
     rdfStoreConfig: RdfStoreConfig,
     timeRecorder:   SparqlQueryTimeRecorder[F]
 ) extends RdfStoreClientImpl(rdfStoreConfig, timeRecorder)
-    with SecurityRecordFinder[F, projects.Path] {
+    with SecurityRecordFinder[F, datasets.Identifier] {
 
-  override def apply(path: projects.Path): F[List[SecurityRecord]] =
-    queryExpecting[List[SecurityRecord]](using = query(path))(recordsDecoder(path))
+  override def apply(id: datasets.Identifier): F[List[SecurityRecord]] =
+    queryExpecting[List[SecurityRecord]](using = query(id))(recordsDecoder)
 
   import eu.timepit.refined.auto._
   import io.renku.graph.model.Schemas._
 
-  private def query(path: projects.Path) = SparqlQuery.of(
-    name = "authorise - project path",
+  private def query(id: datasets.Identifier) = SparqlQuery.of(
+    name = "authorise - dataset id",
     Prefixes.of(schema -> "schema", renku -> "renku"),
-    s"""|SELECT DISTINCT ?projectId ?visibility (GROUP_CONCAT(?maybeMemberGitLabId; separator=',') AS ?memberGitLabIds)
+    s"""|SELECT DISTINCT ?projectId ?path ?visibility (GROUP_CONCAT(?maybeMemberGitLabId; separator=',') AS ?memberGitLabIds)
         |WHERE {
         |  ?projectId a schema:Project;
-        |             renku:projectPath '$path';
+        |             renku:hasDataset/schema:identifier '$id';
+        |             renku:projectPath ?path;
         |             renku:projectVisibility ?visibility.
         |  OPTIONAL {
         |    ?projectId schema:member/schema:sameAs ?sameAsId.
@@ -63,16 +64,18 @@ private class ProjectPathRecordsFinderImpl[F[_]: Async: Logger](
         |              schema:identifier ?maybeMemberGitLabId.
         |  }
         |}
-        |GROUP BY ?projectId ?visibility
+        |GROUP BY ?projectId ?path ?visibility
         |""".stripMargin
   )
 
-  private def recordsDecoder(path: projects.Path): Decoder[List[SecurityRecord]] = {
+  private lazy val recordsDecoder: Decoder[List[SecurityRecord]] = {
     import Decoder._
+    import io.renku.tinytypes.json.TinyTypeDecoders._
 
     val recordDecoder: Decoder[SecurityRecord] = { cursor =>
       for {
         visibility <- cursor.downField("visibility").downField("value").as[Visibility]
+        path       <- cursor.downField("path").downField("value").as[projects.Path]
         maybeUserId <- cursor
                          .downField("memberGitLabIds")
                          .downField("value")
