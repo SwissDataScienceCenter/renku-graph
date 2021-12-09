5d1005b785300552c760c05e9c3b3c83f55068d7
Travis Lee <travis.lee@sdsc.ethz.ch>
2021-12-09 11:37:02 +0100

    fix: fix GitLabClientSpec

--- graph-commons/src/test/scala/io/renku/http/client/GitLabClientSpec.scala
+++ graph-commons/src/test/scala/io/renku/http/client/GitLabClientSpec.scala
@@ -1,20 +1,22 @@
 package io.renku.http.client
 
 import cats.effect.IO
-import cats.implicits.toShow
+import cats.implicits.{catsSyntaxApplicativeErrorId, toShow}
 import com.github.tomakehurst.wiremock.client.MappingBuilder
-import com.github.tomakehurst.wiremock.client.WireMock.{delete, get, okJson, post, put, stubFor}
+import com.github.tomakehurst.wiremock.client.WireMock._
 import io.renku.control.Throttler
-import io.renku.generators.CommonGraphGenerators.{pages, personalAccessTokens}
+import io.renku.generators.CommonGraphGenerators.{oauthAccessTokens, pages, personalAccessTokens}
 import io.renku.generators.Generators.Implicits._
 import io.renku.generators.Generators._
 import io.renku.graph.model.GitLabUrl
+import io.renku.http.client.RestClientError.{UnauthorizedException, UnexpectedResponseException}
 import io.renku.interpreters.TestLogger
 import io.renku.logging.TestExecutionTimeRecorder
 import io.renku.metrics.GitLabApiCallRecorder
 import io.renku.stubbing.ExternalServiceStubbing
 import io.renku.testtools.IOSpec
 import org.http4s.Method._
+import org.http4s.Status.InternalServerError
 import org.http4s.syntax.all._
 import org.http4s.{Method, Uri}
 import org.scalacheck.Gen
@@ -32,7 +34,6 @@ class GitLabClientSpec
   "send" should {
 
     "fetch json from a given page" in new TestCase {
-      val maybeNextPage = pages.generateOption
 
       stubFor {
         callMethod(method, uri)
@@ -45,15 +46,104 @@ class GitLabClientSpec
       client.send(method, uri, endpointName)(_ => IO(result)).unsafeRunSync() shouldBe result
     }
 
-    "fetch commits using the given personal access token" in new TestCase {}
-    "fetch commits using the given oauth token" in new TestCase {}
-    "Handle empty JSON" in new TestCase {}
-    "Handle 404 NOT FOUND without throwing" in new TestCase {}
-    "return an UnauthorizedException if remote client responds with UNAUTHORIZED" in new TestCase {}
-    "return an Exception if remote client responds with status neither OK nor UNAUTHORIZED" in new TestCase {}
-    "return an Exception if remote client responds with unexpected body" in new TestCase {}
+    "fetch commits using the given personal access token" in new TestCase {
+      val personalAccessToken = personalAccessTokens.generateOne
 
-    //TODO: cases for post/delete/put/etc.
+      stubFor {
+        callMethod(method, uri)
+          .withHeader("PRIVATE-TOKEN", equalTo(personalAccessToken.value))
+          .willReturn(
+            okJson(result)
+              .withHeader("X-Next-Page", maybeNextPage.map(_.show).getOrElse(""))
+          )
+      }
+
+      client.send(method, uri, endpointName)(_ => IO(result)).unsafeRunSync() shouldBe result
+    }
+
+    "fetch commits using the given oauth token" in new TestCase {
+      val authAccessToken = oauthAccessTokens.generateOne
+
+      stubFor {
+        callMethod(method, uri)
+          .withHeader("PRIVATE-TOKEN", equalTo(authAccessToken.value))
+          .willReturn(
+            okJson(result)
+              .withHeader("X-Next-Page", maybeNextPage.map(_.show).getOrElse(""))
+          )
+      }
+
+      client.send(method, uri, endpointName)(_ => IO(result)).unsafeRunSync() shouldBe result
+    }
+
+    "Handle empty JSON" in new TestCase {
+      override val result: String = "{}"
+      stubFor {
+        callMethod(method, uri)
+          .willReturn(
+            okJson(result)
+              .withHeader("X-Next-Page", maybeNextPage.map(_.show).getOrElse(""))
+          )
+      }
+
+      client.send(method, uri, endpointName)(_ => IO(result)).unsafeRunSync() shouldBe result
+
+    }
+
+    "Handle 404 NOT FOUND without throwing" in new TestCase {
+      stubFor {
+        callMethod(method, uri)
+          .willReturn(
+            notFound()
+          )
+      }
+
+      client.send(method, uri, endpointName)(_ => IO(())).unsafeRunSync() shouldBe ()
+
+    }
+
+    "return an UnauthorizedException if remote client responds with UNAUTHORIZED" in new TestCase {
+      stubFor {
+        callMethod(method, uri)
+          .willReturn(
+            unauthorized()
+          )
+      }
+
+      intercept[Exception] {
+        client.send(method, uri, endpointName)(_ => UnauthorizedException.raiseError[IO, Unit]).unsafeRunSync()
+      } shouldBe UnauthorizedException
+    }
+
+    "return an Exception if remote client responds with status neither OK nor UNAUTHORIZED" in new TestCase {
+      stubFor {
+        callMethod(method, uri)
+          .willReturn(
+            badRequest()
+          )
+      }
+
+      intercept[Exception] {
+        client
+          .send(method, uri, endpointName)(_ =>
+            UnexpectedResponseException(InternalServerError, "Unexpected response").raiseError[IO, Unit]
+          )
+          .unsafeRunSync()
+      }
+    }
+
+    "return an Exception if remote client responds with unexpected body" in new TestCase {
+      stubFor {
+        callMethod(method, uri)
+          .willReturn(okJson("{}"))
+      }
+
+      intercept[Exception] {
+        client
+          .send(method, uri, endpointName)(_ => new Exception("Unexpected body").raiseError[IO, Unit])
+          .unsafeRunSync()
+      }.getMessage should startWith(s"$method $externalServiceBaseUrl$uri returned 200 OK; error: Unexpected body")
+    }
   }
 
   private trait TestCase {
@@ -64,9 +154,10 @@ class GitLabClientSpec
     val gitLabApiUrl    = GitLabUrl(externalServiceBaseUrl).apiV4
     val client          = new GitLabClientImpl[IO](gitLabApiUrl, apiCallRecorder, Throttler.noThrottling)
 
-    val method       = Gen.oneOf(GET, PUT, DELETE, POST).generateOne
-    val endpointName = nonBlankStrings().generateOne
-    val uri          = uri"" / nonBlankStrings().generateList().mkString("/")
+    val method        = Gen.oneOf(GET, PUT, DELETE, POST).generateOne
+    val endpointName  = nonBlankStrings().generateOne
+    val uri           = uri"" / nonBlankStrings().generateList().mkString("/")
+    val maybeNextPage = pages.generateOption
 
     val result = jsons.generateOne.toString()
 
