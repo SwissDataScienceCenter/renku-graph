785b53ef919ba382e75b1db3c1a21669b90de01e
David Kunzmann <davidkunzmann@gmail.com>
2021-12-16 14:44:25 +0100

    chore: registering clean up subscription

--- event-log/src/main/scala/io/renku/eventlog/subscriptions/EventProducersRegistry.scala
+++ event-log/src/main/scala/io/renku/eventlog/subscriptions/EventProducersRegistry.scala
@@ -86,6 +86,7 @@ object EventProducersRegistry {
                                                                       queriesExecTimes,
                                                                       subscriberTracker
                                 )
+    cleanUpEventCategory <- cleanup.SubscriptionCategory(subscriberTracker, queriesExecTimes)
     zombieEventsCategory <- zombieevents.SubscriptionCategory(sessionResource, queriesExecTimes, subscriberTracker)
   } yield new EventProducersRegistryImpl(
     Set[SubscriptionCategory[F]](
@@ -94,6 +95,7 @@ object EventProducersRegistry {
       commitSyncCategory,
       globalCommitSyncCategory,
       triplesGeneratedCategory,
+      cleanUpEventCategory,
       zombieEventsCategory
     )
   )
