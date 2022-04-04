49021893cbf56d6391c8377112c2ec4ade6fdd25
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2022-04-04 16:37:08 +0200

    feat: migrations for broken Topmost Derived From in TS

--- triples-generator/src/main/scala/io/renku/triplesgenerator/events/categories/tsmigrationrequest/migrations/tooling/QueryBasedMigration.scala
+++ triples-generator/src/main/scala/io/renku/triplesgenerator/events/categories/tsmigrationrequest/migrations/tooling/QueryBasedMigration.scala
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2022 Swiss Data Science Center (SDSC)
+ * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+ * Eidgenössische Technische Hochschule Zürich (ETHZ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.renku.triplesgenerator.events.categories.tsmigrationrequest
 package migrations.tooling
 
@@ -5,10 +23,13 @@ import ConditionedMigration.MigrationRequired
 import QueryBasedMigration.EventData
 import cats.MonadThrow
 import cats.data.EitherT
+import cats.effect.Async
 import cats.syntax.all._
 import io.renku.events.producers.EventSender
 import io.renku.events.{CategoryName, EventRequestContent}
 import io.renku.graph.model.projects
+import io.renku.metrics.MetricsRegistry
+import io.renku.rdfstore.{SparqlQuery, SparqlQueryTimeRecorder}
 import io.renku.triplesgenerator.events.categories.ProcessingRecoverableError
 import org.typelevel.log4cats.Logger
 
@@ -59,5 +80,16 @@ private[migrations] class QueryBasedMigration[F[_]: MonadThrow: Logger](
 }
 
 private[migrations] object QueryBasedMigration {
+
   type EventData = (projects.Path, EventRequestContent.NoPayload, CategoryName)
+
+  def apply[F[_]: Async: Logger: SparqlQueryTimeRecorder: MetricsRegistry](
+      name:          Migration.Name,
+      query:         SparqlQuery,
+      eventProducer: projects.Path => EventData
+  ): F[QueryBasedMigration[F]] = for {
+    recordsFinder     <- RecordsFinder[F](query)
+    eventSender       <- EventSender[F]
+    executionRegister <- MigrationExecutionRegister[F]
+  } yield new QueryBasedMigration[F](name, recordsFinder, eventProducer, eventSender, executionRegister)
 }
