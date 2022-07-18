fef613f0fe94843e9c842da7463f5b56140bfe1d
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2022-07-18 15:01:47 +0200

    fix: acceptance tests Triples Store not to create DS on startup

--- acceptance-tests/src/test/scala/io/renku/graph/acceptancetests/db/TriplesStore.scala
+++ acceptance-tests/src/test/scala/io/renku/graph/acceptancetests/db/TriplesStore.scala
@@ -19,16 +19,10 @@
 package io.renku.graph.acceptancetests.db
 
 import cats.effect.{IO, Temporal}
-import cats.syntax.all._
 import cats.{Applicative, Monad}
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.auto._
 import eu.timepit.refined.numeric.Positive
-import io.renku.graph.acceptancetests.data.RdfStoreData.currentVersionPair
-import io.renku.graph.config.RenkuUrlLoader
-import io.renku.graph.model.RenkuVersionPair
-import io.renku.graph.model.Schemas.renku
-import io.renku.jsonld.EntityId
 import io.renku.rdfstore._
 import org.typelevel.log4cats.Logger
 
@@ -38,39 +32,14 @@ object TriplesStore extends InMemoryJena with RenkuDataset with MigrationsDatase
 
   protected override val maybeJenaFixedPort: Option[Int Refined Positive] = Some(3030)
 
-  def start(
-      maybeVersionPair: Option[RenkuVersionPair] = Some(currentVersionPair)
-  )(implicit logger:    Logger[IO]): IO[Unit] = for {
+  def start()(implicit logger: Logger[IO]): IO[Unit] = for {
     _ <- Applicative[IO].unlessA(isRunning)(IO(container.start()))
     _ <- waitForReadiness
     _ <- logger.info("Triples Store started")
-    _ <- createDatasets()
-    _ <- logger.info("Triples Store datasets created")
-    _ <- updateVersionPair(maybeVersionPair)
   } yield ()
 
   private def isRunning: Boolean = Option(container.containerInfo).exists(_.getState.getRunning)
 
   private def waitForReadiness(implicit logger: Logger[IO]): IO[Unit] =
     Monad[IO].whileM_(IO(!isRunning))(logger.info("Waiting for TS") >> (Temporal[IO] sleep (500 millis)))
-
-  private def updateVersionPair(maybeVersionPair: Option[RenkuVersionPair])(implicit logger: Logger[IO]): IO[Unit] =
-    maybeVersionPair match {
-      case None => IO.unit
-      case Some(versionPair) =>
-        for {
-          renkuUrl <- RenkuUrlLoader[IO]()
-          _ <- runUpdate(
-                 on = migrationsDataset,
-                 SparqlQuery.of(
-                   "version-pair insert",
-                   s"""|INSERT DATA {
-                       |<${EntityId.of((renkuUrl / "version-pair").toString)}> a <${renku / "VersionPair"}> ;
-                       |                                                       <${renku / "cliVersion"}> '${versionPair.cliVersion}' ;
-                       |                                                       <${renku / "schemaVersion"}> '${versionPair.schemaVersion}'}""".stripMargin
-                 )
-               )
-          _ <- logger.info(show"Triples Store on $versionPair")
-        } yield ()
-    }
 }
