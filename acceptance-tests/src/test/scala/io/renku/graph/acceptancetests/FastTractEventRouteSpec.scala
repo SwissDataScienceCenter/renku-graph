29039a5c9957a8fd4da314d9b54f23978bdf2a7c
Eike Kettner <eike.kettner@posteo.de>
2022-09-14 16:52:37 +0200

    feat: Migrate FastTractEventRouteSpec

--- acceptance-tests/src/test/scala/io/renku/graph/acceptancetests/FastTractEventRouteSpec.scala
+++ acceptance-tests/src/test/scala/io/renku/graph/acceptancetests/FastTractEventRouteSpec.scala
@@ -19,15 +19,16 @@
 package io.renku.graph.acceptancetests
 
 import cats.syntax.all._
-import io.renku.generators.CommonGraphGenerators.accessTokens
+import io.renku.generators.CommonGraphGenerators.authUsers
 import io.renku.generators.Generators.Implicits._
 import io.renku.graph.acceptancetests.data.dataProjects
 import io.renku.graph.acceptancetests.flows.TSProvisioning
+import io.renku.graph.acceptancetests.stubs.gitlab.GitLabStubIOSyntax
 import io.renku.graph.acceptancetests.testing.AcceptanceTestPatience
 import io.renku.graph.acceptancetests.tooling.GraphServices
 import io.renku.graph.model.EventsGenerators.commitIds
 import io.renku.graph.model.testentities.generators.EntitiesGenerators.{renkuProjectEntities, visibilityPublic}
-import io.renku.http.client.AccessToken
+import io.renku.http.server.security.model.AuthUser
 import io.renku.webhookservice.model.HookToken
 import org.http4s.Status.{Accepted, Ok}
 import org.scalatest.GivenWhenThen
@@ -40,33 +41,33 @@ class FastTractEventRouteSpec
     with GraphServices
     with TSProvisioning
     with Eventually
-    with AcceptanceTestPatience {
+    with AcceptanceTestPatience
+    with GitLabStubIOSyntax {
 
   Feature("Fast track route for events not in the TS") {
 
     Scenario("Project with no events in the TS") {
 
-      implicit val accessToken: AccessToken = accessTokens.generateOne
+      val user: AuthUser = authUsers.generateOne
       val project  = dataProjects(renkuProjectEntities(visibilityPublic)).generateOne
       val commitId = commitIds.generateOne
 
       Given("commit with the commit id matching Push Event's 'after' exists on the project in GitLab")
-      `GET <gitlabApi>/projects/:id/repository/commits/:sha returning OK with some event`(project, commitId)
-      `GET <gitlabApi>/projects/:id/repository/commits per page returning OK with commits`(project.id, commitId)
+      gitLabStub.addAuthenticated(user)
+      gitLabStub.setupProject(project, commitId)
+      /// `GET <gitlabApi>/projects/:id/repository/commits/:sha returning OK with some event`(project, commitId)
+      // `GET <gitlabApi>/projects/:id/repository/commits per page returning OK with commits`(project.id, commitId)
 
-      `GET <gitlabApi>/projects/:id/events?action=pushed&page=1 returning OK`(project.entitiesProject.maybeCreator,
-                                                                              project,
-                                                                              commitId
-      )
+      // `GET <gitlabApi>/projects/:id/events?action=pushed&page=1 returning OK`(project.entitiesProject.maybeCreator, project, commitId )
 
       And("Triples are failing on generation")
       `GET <triples-generator>/projects/:id/commits/:id fails non recoverably`(project, commitId)
 
       And("project exists in GitLab")
-      `GET <gitlabApi>/projects/:path AND :id returning OK with`(project)
+      // `GET <gitlabApi>/projects/:path AND :id returning OK with`(project)
 
       And("access token is present")
-      givenAccessTokenPresentFor(project)
+      givenAccessTokenPresentFor(project)(user.accessToken)
 
       When("a Push Event arrives")
       webhookServiceClient
