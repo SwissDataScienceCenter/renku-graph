a0b5db05327166644169f4d98f37a4a08416f841
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2023-01-03 19:57:03 +0100

    refactor: graph-commons test' Quad decommissioning

--- graph-commons/src/test/scala/io/renku/triplesstore/Quad.scala
+++ graph-commons/src/test/scala/io/renku/triplesstore/Quad.scala
@@ -18,53 +18,53 @@
 
 package io.renku.triplesstore
 
-import cats.Show
-import cats.syntax.all._
-import io.renku.graph.model.views.SparqlLiteralEncoder.sparqlEncode
-import io.renku.jsonld.{EntityId, EntityIdEncoder, Property}
-import io.renku.tinytypes.{StringTinyType, TinyType}
-
-final case class Quad(graphId: EntityId, triple: Triple)
-
-object Quad {
-
-  import Triple._
-
-  def apply(graphId: EntityId, subject: EntityId, predicate: Property, `object`: String): Quad =
-    Quad(graphId, Value(subject, predicate, s"'${sparqlEncode(`object`)}'"))
-
-  def apply[O <: StringTinyType](graphId: EntityId, subject: EntityId, predicate: Property, `object`: O)(implicit
-      oShow:                              Show[O]
-  ): Quad = apply(graphId, subject, predicate, `object`.show)
-
-  def apply[S, O <: TinyType](graphId: EntityId, subject: S, predicate: Property, `object`: O)(implicit
-      sEncoder:                        EntityIdEncoder[S],
-      oShow:                           Show[O]
-  ): Quad = apply(graphId, subject, predicate, `object`.show)
-
-  def apply[S](graphId: EntityId, subject: S, predicate: Property, `object`: String)(implicit
-      sEncoder:         EntityIdEncoder[S]
-  ): Quad = Quad(graphId, Value(sEncoder(subject), predicate, s"'${sparqlEncode(`object`)}'"))
-
-  def edge[S, O](graphId: EntityId, subject: S, predicate: Property, `object`: O)(implicit
-      sEncoder:           EntityIdEncoder[S],
-      oEncoder:           EntityIdEncoder[O]
-  ): Quad = Quad(graphId, Edge(sEncoder(subject), predicate, oEncoder(`object`)))
-
-  def edge[O](graphId: EntityId, subject: EntityId, predicate: Property, `object`: O)(implicit
-      oEncoder:        EntityIdEncoder[O]
-  ): Quad = Quad(graphId, Edge(subject, predicate, oEncoder(`object`)))
-
-  def edge[S](graphId: EntityId, subject: S, predicate: Property, `object`: EntityId)(implicit
-      sEncoder:        EntityIdEncoder[S]
-  ): Quad = Quad(graphId, Edge(sEncoder(subject), predicate, `object`))
-
-  def edge(graphId: EntityId, subject: EntityId, predicate: Property, `object`: EntityId): Quad =
-    Quad(graphId, Edge(subject, predicate, `object`))
-
-  implicit def show: Show[Quad] = Show.show { quad =>
-    show"""|GRAPH <${quad.graphId.show}> {
-           |  ${quad.triple}
-           |}""".stripMargin
-  }
-}
+//import cats.Show
+//import cats.syntax.all._
+//import io.renku.graph.model.views.SparqlLiteralEncoder.sparqlEncode
+//import io.renku.jsonld.{EntityId, EntityIdEncoder, Property}
+//import io.renku.tinytypes.{StringTinyType, TinyType}
+//
+//final case class Quad(graphId: EntityId, triple: Triple)
+//
+//object Quad {
+//
+//  import Triple._
+//
+//  def apply(graphId: EntityId, subject: EntityId, predicate: Property, `object`: String): Quad =
+//    Quad(graphId, Value(subject, predicate, s"'${sparqlEncode(`object`)}'"))
+//
+//  def apply[O <: StringTinyType](graphId: EntityId, subject: EntityId, predicate: Property, `object`: O)(implicit
+//      oShow:                              Show[O]
+//  ): Quad = apply(graphId, subject, predicate, `object`.show)
+//
+//  def apply[S, O <: TinyType](graphId: EntityId, subject: S, predicate: Property, `object`: O)(implicit
+//      sEncoder:                        EntityIdEncoder[S],
+//      oShow:                           Show[O]
+//  ): Quad = apply(graphId, subject, predicate, `object`.show)
+//
+//  def apply[S](graphId: EntityId, subject: S, predicate: Property, `object`: String)(implicit
+//      sEncoder:         EntityIdEncoder[S]
+//  ): Quad = Quad(graphId, Value(sEncoder(subject), predicate, s"'${sparqlEncode(`object`)}'"))
+//
+//  def edge[S, O](graphId: EntityId, subject: S, predicate: Property, `object`: O)(implicit
+//      sEncoder:           EntityIdEncoder[S],
+//      oEncoder:           EntityIdEncoder[O]
+//  ): Quad = Quad(graphId, Edge(sEncoder(subject), predicate, oEncoder(`object`)))
+//
+//  def edge[O](graphId: EntityId, subject: EntityId, predicate: Property, `object`: O)(implicit
+//      oEncoder:        EntityIdEncoder[O]
+//  ): Quad = Quad(graphId, Edge(subject, predicate, oEncoder(`object`)))
+//
+//  def edge[S](graphId: EntityId, subject: S, predicate: Property, `object`: EntityId)(implicit
+//      sEncoder:        EntityIdEncoder[S]
+//  ): Quad = Quad(graphId, Edge(sEncoder(subject), predicate, `object`))
+//
+//  def edge(graphId: EntityId, subject: EntityId, predicate: Property, `object`: EntityId): Quad =
+//    Quad(graphId, Edge(subject, predicate, `object`))
+//
+//  implicit def show: Show[Quad] = Show.show { quad =>
+//    show"""|GRAPH <${quad.graphId.show}> {
+//           |  ${quad.triple}
+//           |}""".stripMargin
+//  }
+//}
