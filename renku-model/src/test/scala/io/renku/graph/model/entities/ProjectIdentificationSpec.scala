e28a427026a3c3e9747159fe95053a314e505805
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2023-01-17 11:22:42 +0100

    feat: ProjectIdentification class

---
+++ renku-model/src/test/scala/io/renku/graph/model/entities/ProjectIdentificationSpec.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2023 Swiss Data Science Center (SDSC)
+ * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+ * Eidgenössische Technische Hochschule Zürich (ETHZ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.renku.graph.model.entities
+
+import io.renku.generators.Generators.Implicits._
+import io.renku.graph.model.entities
+import io.renku.graph.model.testentities._
+import org.scalatest.matchers.should
+import org.scalatest.wordspec.AnyWordSpec
+
+class ProjectIdentificationSpec extends AnyWordSpec with should.Matchers {
+
+  "apply" should {
+
+    "return the ProjectIdentification from the given Project" in {
+
+      val project = anyProjectEntities.generateOne.to[entities.Project]
+
+      ProjectIdentification(project) shouldBe ProjectIdentification(project.resourceId, project.path)
+    }
+  }
+}
