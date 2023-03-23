8bd6929ecb89e2f1d34130b719daa1396c416d6e
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2023-03-23 17:45:06 +0100

    feat: sparql string interpolator to resolve iterables considering context

---
+++ triples-store-client/src/main/scala/io/renku/triplesstore/client/sparql/StringInterpolator.scala
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2023 Swiss Data Science Center (SDSC)
+ * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+ * Eidgenössische Technische Hochschule Zürich (ETHZ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.renku.triplesstore.client.sparql
+
+import io.renku.jsonld.{EntityId, Property}
+import io.renku.triplesstore.client.model.{Quad, Triple, TripleObject}
+import io.renku.triplesstore.client.syntax._
+
+import java.time.{Instant, LocalDate}
+
+class StringInterpolator(private val sc: StringContext) {
+
+  import StringInterpolator.ClauseDetector
+
+  def sparql(args: Any*): Fragment = fr(args: _*)
+
+  def fr(args: Any*): Fragment = {
+    val values = args.zipWithIndex.map(makeValue)
+    Fragment(sc.s(values: _*))
+  }
+
+  private lazy val makeValue: ((Any, Int)) => String = {
+    case (a: LuceneQuery, _)      => a.asSparql.sparql
+    case (a: String, _)           => a.asTripleObject.asSparql.sparql
+    case (a: Char, _)             => a.toString.asTripleObject.asSparql.sparql
+    case (a: Float, _)            => a.asTripleObject.asSparql.sparql
+    case (a: Long, _)             => a.asTripleObject.asSparql.sparql
+    case (a: Double, _)           => a.asTripleObject.asSparql.sparql
+    case (a: Boolean, _)          => a.asTripleObject.asSparql.sparql
+    case (a: Instant, _)          => a.asTripleObject.asSparql.sparql
+    case (a: LocalDate, _)        => a.asTripleObject.asSparql.sparql
+    case (a: EntityId, _)         => a.asSparql.sparql
+    case (a: Triple, _)           => a.asSparql.sparql
+    case (a: Quad, _)             => a.asSparql.sparql
+    case (a: TripleObject, _)     => a.asSparql.sparql
+    case (a: Property, _)         => a.asSparql.sparql
+    case (a: Fragment, _)         => a.sparql
+    case (a: VarName, _)          => a.name
+    case (it: Iterable[Any], idx) => resolveIterable(it, idx)
+    case (opt: Option[Any], idx)  => opt.map(makeValue(_, idx)).getOrElse("")
+    case (arg, _)                 => sys.error(s"Unsupported value type '${arg.getClass}: $arg'")
+  }
+
+  private def resolveIterable(it: Iterable[Any], idx: Int) = {
+    import ClauseDetector.ClauseType
+    ClauseDetector
+      .detectClauseContext(sc.parts(idx))
+      .map {
+        case ClauseType.VALUES => it.map(makeValue(_, idx)).map(s => s"($s)").mkString(" ")
+        case ClauseType.IN     => it.map(makeValue(_, idx)).mkString(", ")
+      }
+      .getOrElse(sys.error("Iterable cannot be resolved in this context"))
+  }
+}
+
+private object StringInterpolator {
+
+  private object ClauseDetector {
+    private val valuesClause = ".*VALUES[\\s]*\\([\\s]*\\?\\w+[\\s]*\\)[\\s]*\\{[\\s]*$".r
+    private val inClause     = ".*IN[\\s]*\\([\\s]*$".r
+
+    sealed trait ClauseType
+    object ClauseType {
+      case object VALUES extends ClauseType
+      case object IN     extends ClauseType
+    }
+
+    def detectClauseContext(snippet: String): Option[ClauseType] =
+      if (valuesClause matches snippet.toUpperCase) Some(ClauseType.VALUES)
+      else if (inClause.matches(snippet.toUpperCase)) Some(ClauseType.IN)
+      else Option.empty
+  }
+}
---
+++ triples-store-client/src/test/scala/io/renku/triplesstore/client/sparql/StringInterpolatorSpec.scala
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2023 Swiss Data Science Center (SDSC)
+ * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+ * Eidgenössische Technische Hochschule Zürich (ETHZ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.renku.triplesstore.client
+package sparql
+
+import io.renku.generators.Generators.{localDates, nonEmptyStrings, timestamps}
+import io.renku.generators.Generators.Implicits._
+import io.renku.generators.jsonld.JsonLDGenerators.{entityIds, properties}
+import org.scalacheck.Arbitrary
+import org.scalatest.matchers.should
+import org.scalatest.wordspec.AnyWordSpec
+import TriplesStoreGenerators._
+import syntax._
+
+class StringInterpolatorSpec extends AnyWordSpec with should.Matchers {
+
+  "fr" should {
+
+    "encode a LuceneQuery if used" in {
+      val value = LuceneQuery(s"{${nonEmptyStrings(minLength = 3).generateOne}")
+      fr"$value" shouldBe value.asSparql
+    }
+
+    "encode a String if used" in {
+      val value = nonEmptyStrings().generateOne
+      fr"$value" shouldBe value.asTripleObject.asSparql
+    }
+
+    "encode a Char if used" in {
+      val value = Arbitrary.arbChar.arbitrary.generateOne
+      fr"$value" shouldBe value.toString.asTripleObject.asSparql
+    }
+
+    "encode a Float if used" in {
+      val value = Arbitrary.arbFloat.arbitrary.generateOne
+      fr"$value" shouldBe value.asTripleObject.asSparql
+    }
+
+    "encode a Long if used" in {
+      val value = Arbitrary.arbLong.arbitrary.generateOne
+      fr"$value" shouldBe value.asTripleObject.asSparql
+    }
+
+    "encode a Double if used" in {
+      val value = Arbitrary.arbDouble.arbitrary.generateOne
+      fr"$value" shouldBe value.asTripleObject.asSparql
+    }
+
+    "encode a Boolean if used" in {
+      val value = Arbitrary.arbBool.arbitrary.generateOne
+      fr"$value" shouldBe value.asTripleObject.asSparql
+    }
+
+    "encode an Instant if used" in {
+      val value = timestamps.generateOne
+      fr"$value" shouldBe value.asTripleObject.asSparql
+    }
+
+    "encode a LocalDate if used" in {
+      val value = localDates.generateOne
+      fr"$value" shouldBe value.asTripleObject.asSparql
+    }
+
+    "encode a EntityId if used" in {
+      val value = entityIds.generateOne
+      fr"$value" shouldBe value.asTripleObject.asSparql
+    }
+
+    "encode a Triple if used" in {
+      val value = triples.generateOne
+      fr"$value" shouldBe value.asSparql
+    }
+
+    "encode a Quad if used" in {
+      val value = quads.generateOne
+      fr"$value" shouldBe value.asSparql
+    }
+
+    "encode a TripleObject if used" in {
+      val value = tripleObjects.generateOne
+      fr"$value" shouldBe value.asSparql
+    }
+
+    "encode a Property if used" in {
+      val value = properties.generateOne
+      fr"$value" shouldBe value.asSparql
+    }
+
+    "encode a Fragment if used" in {
+      val value = fr"${properties.generateOne}"
+      fr"$value" shouldBe value
+    }
+
+    "encode a VarName if used" in {
+      val value = VarName(s"?${nonEmptyStrings().generateOne}")
+      fr"$value" shouldBe Fragment(value.name)
+    }
+
+    "encode an Iterable if used in a context of VALUES clause" in {
+      val value = nonEmptyStrings().generateNonEmptyList().toList
+      fr"VALUES (?v) { $value }" shouldBe Fragment(
+        s"VALUES (?v) { ${value.map(v => fr"$v".sparql).map(s => s"($s)").mkString(" ")} }"
+      )
+    }
+
+    "encode an Iterable if used in a context of IN clause" in {
+      val value = nonEmptyStrings().generateNonEmptyList().toList
+      fr"IN ($value)" shouldBe Fragment(
+        s"IN (${value.map(v => fr"$v".sparql).mkString(", ")})"
+      )
+    }
+
+    "fail if an Iterable is used in a context other than VALUES or IN" in {
+      val value = nonEmptyStrings().generateNonEmptyList().toList
+      intercept[Exception](fr"some ($value)").getMessage should startWith("Iterable cannot be resolved in this context")
+    }
+
+    "encode a non empty Option if used" in {
+      val value = nonEmptyStrings().generateOne
+      fr"${Option(value)}" shouldBe fr"$value"
+    }
+
+    "encode an empty Option if used" in {
+      fr"${Option.empty}" shouldBe fr""
+    }
+
+    "fail if a value of unsupported type is used" in {
+      intercept[Exception](fr"${new {}}").getMessage should startWith("Unsupported value type")
+    }
+  }
+}
