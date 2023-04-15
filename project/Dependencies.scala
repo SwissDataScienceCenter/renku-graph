78b3ddfdffa8b4fdce908b8996a397b3669c6791
RenkuBot <53332360+RenkuBot@users.noreply.github.com>
2023-04-15 20:13:05 +0200

    chore: Update pureconfig, pureconfig-cats from 0.17.2 to 0.17.3 (#1427)
    Co-authored-by: RenkuBot <RenkuBot@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
     val luceneQueryParser      = "9.5.0"
     val monocle                = "2.1.0"
     val owlapi                 = "5.5.0"
-    val pureconfig             = "0.17.2"
+    val pureconfig             = "0.17.3"
     val rdf4jQueryParserSparql = "4.2.3"
     val refined                = "0.10.3"
     val refinedPureconfig      = "0.10.3"
