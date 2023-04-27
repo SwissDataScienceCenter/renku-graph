e3f7c32dd341584b9e46739c7e15f82499cb3c68
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2023-04-27 14:17:13 +0200

    fix: cli dev version installation command

--- triples-generator/entrypoint.sh
+++ triples-generator/entrypoint.sh
@@ -3,7 +3,7 @@
 if [ ! -z "$RENKU_PYTHON_DEV_VERSION" ]
 then
     /usr/bin/python3 -m pip uninstall --yes renku && \
-    /usr/bin/python3 -m pip install 'renku==${RENKU_PYTHON_DEV_VERSION}' &
+    /usr/bin/python3 -m pip install renku==${RENKU_PYTHON_DEV_VERSION} &
 fi
 
 # run the command
