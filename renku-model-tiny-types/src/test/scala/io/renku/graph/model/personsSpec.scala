3213f954cbe11fdb2bfa1ac5c785165c8a2352c2
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2023-05-04 09:20:19 +0200

    test: spec verifying Email is Sensitive

--- renku-model-tiny-types/src/test/scala/io/renku/graph/model/personsSpec.scala
+++ renku-model-tiny-types/src/test/scala/io/renku/graph/model/personsSpec.scala
@@ -23,6 +23,7 @@ import io.renku.generators.Generators.Implicits._
 import io.renku.generators.Generators._
 import io.renku.graph.model.persons.{Email, ResourceId}
 import io.renku.graph.model.views.RdfResource
+import io.renku.tinytypes.Sensitive
 import io.renku.tinytypes.constraints.NonBlank
 import org.apache.jena.util.URIref
 import org.scalacheck.Gen
@@ -37,6 +38,10 @@ class EmailSpec extends AnyWordSpec with ScalaCheckPropertyChecks with should.Ma
 
     "be a NonBlank" in {
       Email shouldBe a[NonBlank[_]]
+    }
+
+    "be Sensitive" in {
+      personEmails.generateOne shouldBe a[Sensitive]
     }
   }
 
