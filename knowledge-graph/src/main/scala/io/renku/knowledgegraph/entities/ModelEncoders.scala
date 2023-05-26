24b0afce7dfc41ea8995a52360269b617cdb1512
Eike Kettner <eike.kettner@sdsc.ethz.ch>
2023-05-26 14:51:27 +0200

    Revert back to returning only the "slug" value
    The new property "slug" is introduced to replace "name". But for the
    transition period, both will transport the same value.

--- knowledge-graph/src/main/scala/io/renku/knowledgegraph/entities/ModelEncoders.scala
+++ knowledge-graph/src/main/scala/io/renku/knowledgegraph/entities/ModelEncoders.scala
@@ -98,7 +98,7 @@ trait ModelEncoders {
       json"""{
         "type":          ${Criteria.Filters.EntityType.Dataset.value},
         "matchingScore": ${ds.matchingScore},
-        "name":          ${ds.name},
+        "name":          ${ds.slug},
         "slug":          ${ds.slug},
         "visibility":    ${ds.visibility},
         "date":          ${ds.date},
