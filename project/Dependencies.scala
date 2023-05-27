1dafa81c4fd19276ff21cb13669ae1ee2cd04319
RenkuBot <RenkuBot@users.noreply.github.com>
2023-05-27 06:02:38 +0000

    chore: Update testcontainers-scala-postgresql, ... from 0.40.15 to 0.40.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -34,7 +34,7 @@ object Dependencies {
     val sentryLogback          = "6.19.1"
     val skunk                  = "0.5.1"
     val swaggerParser          = "2.1.14"
-    val testContainersScala    = "0.40.15"
+    val testContainersScala    = "0.40.16"
     val widoco                 = "1.4.18"
     val wiremock               = "2.35.0"
   }
