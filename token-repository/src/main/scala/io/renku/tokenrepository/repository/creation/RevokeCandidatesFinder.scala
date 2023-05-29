8971b73797ee9061a68fff49a399873490d9594f
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2023-05-29 11:27:40 +0200

    fix: token revoking process to read all project tokens

--- token-repository/src/main/scala/io/renku/tokenrepository/repository/creation/RevokeCandidatesFinder.scala
+++ token-repository/src/main/scala/io/renku/tokenrepository/repository/creation/RevokeCandidatesFinder.scala
@@ -23,6 +23,7 @@ import cats.effect.Async
 import cats.syntax.all._
 import io.renku.graph.model.projects
 import io.renku.http.client.{AccessToken, GitLabClient}
+import io.renku.http.rest.paging.model.{Page, PerPage}
 
 import java.time.LocalDate.now
 import java.time.Period
@@ -34,32 +35,58 @@ private trait RevokeCandidatesFinder[F[_]] {
 private object RevokeCandidatesFinder {
   def apply[F[_]: Async: GitLabClient]: F[RevokeCandidatesFinder[F]] =
     (ProjectTokenDuePeriod[F](), RenkuAccessTokenName[F]())
-      .mapN(new RevokeCandidatesFinderImpl[F](_, _))
+      .mapN(new RevokeCandidatesFinderImpl[F](PerPage(50), _, _))
 }
 
-private class RevokeCandidatesFinderImpl[F[_]: Async: GitLabClient](tokenDuePeriod: Period,
+private class RevokeCandidatesFinderImpl[F[_]: Async: GitLabClient](pageSize: PerPage,
+                                                                    tokenDuePeriod: Period,
                                                                     renkuTokenName: RenkuAccessTokenName
 ) extends RevokeCandidatesFinder[F] {
 
+  import eu.timepit.refined.api.Refined
   import eu.timepit.refined.auto._
+  import eu.timepit.refined.collection.NonEmpty
+  import fs2.Stream
   import io.circe.Decoder
   import io.circe.Decoder._
+  import io.renku.http.tinytypes.TinyTypeURIEncoder._
   import org.http4s.Status.{Forbidden, NotFound, Ok, Unauthorized}
   import org.http4s.circe.jsonOf
   import org.http4s.implicits._
   import org.http4s.{EntityDecoder, Request, Response, Status}
+  import org.typelevel.ci._
+
+  private val endpointName: String Refined NonEmpty = "project-access-tokens"
 
   override def findTokensToRemove(projectId: projects.GitLabId, accessToken: AccessToken): F[List[AccessTokenId]] =
+    Stream
+      .iterate(1)(_ + 1)
+      .evalMap(fetch(_, projectId, accessToken))
+      .map { case (tokens, maybeNextPage) =>
+        tokens.filter(renkuTokens).filter(dueToRefresh).map(_._1) -> maybeNextPage
+      }
+      .takeThrough { case (_, maybeNextPage) => maybeNextPage.nonEmpty }
+      .map(_._1)
+      .compile
+      .toList
+      .map(_.flatten)
+
+  private def fetch(page: Int, projectId: projects.GitLabId, accessToken: AccessToken) =
     GitLabClient[F]
-      .get(uri"projects" / projectId.value / "access_tokens", "project-access-tokens")(mapResponse)(accessToken.some)
-      .map(_.filter(renkuTokens).filter(dueToRefresh).map(_._1))
+      .get(uri"projects" / projectId / "access_tokens" +? ("per_page" -> pageSize) +? ("page" -> page), endpointName)(
+        mapResponse
+      )(accessToken.some)
 
   private type TokenInfo = (AccessTokenId, String, Option[ExpiryDate])
 
-  private lazy val mapResponse: PartialFunction[(Status, Request[F], Response[F]), F[List[TokenInfo]]] = {
-    case (Ok, _, response)                           => response.as[List[TokenInfo]]
-    case (Unauthorized | Forbidden | NotFound, _, _) => List.empty[TokenInfo].pure[F]
+  private lazy val mapResponse
+      : PartialFunction[(Status, Request[F], Response[F]), F[(List[TokenInfo], Option[Page])]] = {
+    case (Ok, _, response)                           => response.as[List[TokenInfo]].map(_ -> maybeNextPage(response))
+    case (Unauthorized | Forbidden | NotFound, _, _) => (List.empty[TokenInfo] -> Option.empty[Page]).pure[F]
   }
+
+  private def maybeNextPage(response: Response[F]): Option[Page] =
+    response.headers.get(ci"X-Next-Page").flatMap(_.head.value.toIntOption.map(Page))
 
   private implicit lazy val decoder: EntityDecoder[F, List[TokenInfo]] = {
     import io.renku.tinytypes.json.TinyTypeDecoders._
