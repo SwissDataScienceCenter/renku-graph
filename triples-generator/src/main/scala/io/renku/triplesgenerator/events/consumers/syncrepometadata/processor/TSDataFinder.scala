9a038bde964a27445b887af3e0a162bda5cd9f17
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2023-06-14 15:27:12 +0200

    feat: TSDataFinder

--- triples-generator/src/main/scala/io/renku/triplesgenerator/events/consumers/syncrepometadata/processor/TSDataFinder.scala
+++ triples-generator/src/main/scala/io/renku/triplesgenerator/events/consumers/syncrepometadata/processor/TSDataFinder.scala
@@ -16,10 +16,52 @@
  * limitations under the License.
  */
 
-package io.renku.triplesgenerator.events.consumers.syncrepometadata.processor
+package io.renku.triplesgenerator.events.consumers.syncrepometadata
+package processor
 
+import cats.MonadThrow
+import cats.syntax.all._
+import eu.timepit.refined.auto._
+import io.renku.graph.model.Schemas.{renku, schema}
 import io.renku.graph.model.projects
+import io.renku.triplesstore.SparqlQuery.Prefixes
+import io.renku.triplesstore.client.syntax._
+import io.renku.triplesstore.{ResultsDecoder, SparqlQuery, TSClient}
 
 private trait TSDataFinder[F[_]] {
   def fetchTSData(path: projects.Path): F[Option[DataExtract]]
+}
+
+private class TSDataFinderImpl[F[_]: MonadThrow](tsClient: TSClient[F]) extends TSDataFinder[F] {
+
+  import ResultsDecoder._
+  import io.circe.Decoder
+
+  override def fetchTSData(path: projects.Path): F[Option[DataExtract]] =
+    tsClient.queryExpecting[Option[DataExtract]] {
+      SparqlQuery.ofUnsafe(
+        show"$categoryName: find data",
+        Prefixes of (renku -> "renku", schema -> "schema"),
+        sparql"""|SELECT ?path ?name
+                 |WHERE {
+                 |  BIND (${path.asObject} AS ?path)
+                 |  GRAPH ?id {
+                 |    ?id a schema:Project;
+                 |        renku:projectPath ?path;
+                 |        schema:name ?name
+                 |  }
+                 |}
+                 |LIMIT 1
+                 |""".stripMargin
+      )
+    }(decoder(path))
+
+  private def decoder(path: projects.Path): Decoder[Option[DataExtract]] =
+    ResultsDecoder[Option, DataExtract] { implicit cur =>
+      import io.renku.tinytypes.json.TinyTypeDecoders._
+      for {
+        path <- extract[projects.Path]("path")
+        name <- extract[projects.Name]("name")
+      } yield DataExtract(path, name)
+    }(toOption(show"Multiple projects or values for '$path'"))
 }
