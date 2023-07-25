58054f1bc6bd3d736ef876785110ce69ffe63b1c
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2023-07-25 18:47:32 +0200

    fix: DBUpdater for ToFailure event to retry event in the onRollback

--- event-log/src/main/scala/io/renku/eventlog/events/consumers/statuschange/StatusChanger.scala
+++ event-log/src/main/scala/io/renku/eventlog/events/consumers/statuschange/StatusChanger.scala
@@ -50,13 +50,12 @@ private[statuschange] class StatusChangerImpl[F[_]: MonadCancelThrow: SessionRes
         {
           for {
             savepoint <- Kleisli.liftF(transaction.savepoint)
-            updateResults <-
+            results <-
               dbUpdater
                 .updateDB(event)
-                .flatMapF(res => transaction.commit.as(res))
+                .flatMapF(transaction.commit.as(_))
                 .handleErrorWith(rollback(transaction)(savepoint)(event)(dbUpdater))
-            _ <-
-              Kleisli.liftF(updateGauges(updateResults).handleErrorWith(Logger[F].error(_)("Updating gauges failed")))
+            _ <- Kleisli.liftF(updateGauges(results).handleErrorWith(Logger[F].error(_)("Updating gauges failed")))
           } yield ()
         } run session
       }
