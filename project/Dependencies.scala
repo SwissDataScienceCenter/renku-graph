41950b414b08f7679d1229aee5e2c7ce702c0f46
RenkuBot <RenkuBot@users.noreply.github.com>
2023-09-07 06:03:06 +0000

    chore: Update sentry-logback from 6.28.0 to 6.29.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,7 +32,7 @@ object Dependencies {
     val scalamock              = "5.2.0"
     val scalatest              = "3.2.16"
     val scalatestScalacheck    = "3.2.14.0"
-    val sentryLogback          = "6.28.0"
+    val sentryLogback          = "6.29.0"
     val skunk                  = "0.6.0"
     val swaggerParser          = "2.1.16"
     val testContainersScala    = "0.41.0"
