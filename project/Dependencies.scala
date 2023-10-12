af8449fda5973db5d424d1c7074ab9aa696f955e
RenkuBot <RenkuBot@users.noreply.github.com>
2023-10-12 06:02:21 +0000

    chore: Update skunk-core from 0.6.0 to 0.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -34,7 +34,7 @@ object Dependencies {
     val scalatest              = "3.2.17"
     val scalatestScalacheck    = "3.2.14.0"
     val sentryLogback          = "6.30.0"
-    val skunk                  = "0.6.0"
+    val skunk                  = "0.6.1"
     val swaggerParser          = "2.1.16"
     val testContainersScala    = "0.41.0"
     val widoco                 = "1.4.20"
