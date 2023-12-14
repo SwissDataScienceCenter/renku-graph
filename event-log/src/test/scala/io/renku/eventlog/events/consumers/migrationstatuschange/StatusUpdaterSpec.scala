4156aacd48086ef2fc9d0b80ba047e685eb4c163
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2023-12-14 19:09:32 +0100

    refactor: tsmigration request and status change

--- event-log/src/test/scala/io/renku/eventlog/events/consumers/migrationstatuschange/StatusUpdaterSpec.scala
+++ event-log/src/test/scala/io/renku/eventlog/events/consumers/migrationstatuschange/StatusUpdaterSpec.scala
@@ -19,34 +19,35 @@
 package io.renku.eventlog.events.consumers.migrationstatuschange
 
 import cats.effect.IO
+import cats.effect.testing.scalatest.AsyncIOSpec
 import cats.syntax.all._
-import io.renku.eventlog._
+import io.renku.db.DBConfigProvider.DBConfig
 import io.renku.eventlog.MigrationStatus._
 import io.renku.eventlog.TSMigrationGenerators.changeDates
+import io.renku.eventlog._
 import io.renku.eventlog.events.consumers.migrationstatuschange.Event.{ToDone, ToNonRecoverableFailure, ToRecoverableFailure}
 import io.renku.eventlog.events.producers.tsmigrationrequest.TsMigrationTableProvisioning
-import io.renku.eventlog.metrics.QueriesExecutionTimes
+import io.renku.eventlog.metrics.{QueriesExecutionTimes, TestQueriesExecutionTimes}
 import io.renku.events.Generators.subscriberUrls
 import io.renku.generators.CommonGraphGenerators.serviceVersions
 import io.renku.generators.Generators.Implicits._
 import io.renku.generators.Generators.sentences
-import io.renku.metrics.TestMetricsRegistry
-import io.renku.testtools.IOSpec
 import org.scalacheck.Gen.asciiPrintableChar
-import org.scalamock.scalatest.MockFactory
+import org.scalamock.scalatest.AsyncMockFactory
+import org.scalatest.Succeeded
 import org.scalatest.matchers.should
 import org.scalatest.prop.TableDrivenPropertyChecks
-import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.wordspec.AsyncWordSpec
 
 import java.time.Instant
 import java.time.temporal.ChronoUnit.MICROS
 
 class StatusUpdaterSpec
-    extends AnyWordSpec
-    with IOSpec
-    with InMemoryEventLogDbSpec
+    extends AsyncWordSpec
+    with AsyncIOSpec
+    with EventLogPostgresSpec
+    with AsyncMockFactory
     with TsMigrationTableProvisioning
-    with MockFactory
     with TableDrivenPropertyChecks
     with should.Matchers {
 
@@ -57,67 +58,66 @@ class StatusUpdaterSpec
       ToRecoverableFailure(url, version, message),
       ToNonRecoverableFailure(url, version, message)
     ) foreach { event =>
-      s"succeed updating to ${event.newStatus} only if corresponding record in Sent" in new TestCase {
+      s"succeed updating to ${event.newStatus} only if corresponding record in Sent" in testDBResource.use {
+        implicit cfg =>
+          for {
+            _ <- insertSubscriptionRecord(url, version, Sent, changeDate).assertNoException
 
-        insertSubscriptionRecord(url, version, Sent, changeDate).unsafeRunSync()
+            _ <- updater.updateStatus(event).assertNoException
 
-        updater.updateStatus(event).unsafeRunSync() shouldBe ()
+            _ <- findRow(url, version).asserting(_ shouldBe event.newStatus -> ChangeDate(now))
 
-        findRow(url, version).unsafeRunSync() shouldBe event.newStatus -> ChangeDate(now)
-
-        if (Set(RecoverableFailure, NonRecoverableFailure) contains event.newStatus)
-          findMessage(url, version) shouldBe message.some
-        else
-          findMessage(url, version) shouldBe None
+            _ <- if (Set(RecoverableFailure, NonRecoverableFailure) contains event.newStatus)
+                   findMessage(url, version).asserting(_ shouldBe message.some)
+                 else
+                   findMessage(url, version).asserting(_ shouldBe None)
+          } yield Succeeded
       }
     }
 
     MigrationStatus.all - Sent foreach { status =>
-      s"do nothing and succeed if corresponding record is not in $status" in new TestCase {
+      s"do nothing and succeed if corresponding record is not in $status" in testDBResource.use { implicit cfg =>
+        for {
+          _ <- insertSubscriptionRecord(url, version, status, changeDate).assertNoException
 
-        insertSubscriptionRecord(url, version, status, changeDate).unsafeRunSync()
+          _ <- updater.updateStatus(ToDone(url, version)).assertNoException
 
-        updater.updateStatus(ToDone(url, version)).unsafeRunSync() shouldBe ()
-
-        findRow(url, version).unsafeRunSync() shouldBe status -> changeDate
+          _ <- findRow(url, version).asserting(_ shouldBe status -> changeDate)
+        } yield Succeeded
       }
     }
 
-    "do nothing and succeed if corresponding record does not exist" in new TestCase {
-      updater.updateStatus(ToDone(url, version)).unsafeRunSync() shouldBe ()
+    "do nothing and succeed if corresponding record does not exist" in testDBResource.use { implicit cfg =>
+      updater.updateStatus(ToDone(url, version)).assertNoException
     }
 
-    "do update only corresponding record" in new TestCase {
+    "do update only corresponding record" in testDBResource.use { implicit cfg =>
+      for {
+        _ <- insertSubscriptionRecord(url, version, Sent, changeDate).assertNoException
 
-      insertSubscriptionRecord(url, version, Sent, changeDate).unsafeRunSync()
+        otherUrl = subscriberUrls.generateOne
+        _ <- insertSubscriptionRecord(otherUrl, version, Sent, changeDate).assertNoException
 
-      val otherUrl = subscriberUrls.generateOne
-      insertSubscriptionRecord(otherUrl, version, Sent, changeDate).unsafeRunSync()
+        otherVersion = serviceVersions.generateOne
+        _ <- insertSubscriptionRecord(url, otherVersion, Sent, changeDate).assertNoException
 
-      val otherVersion = serviceVersions.generateOne
-      insertSubscriptionRecord(url, otherVersion, Sent, changeDate).unsafeRunSync()
+        _ <- updater.updateStatus(ToDone(url, version)).assertNoException
 
-      updater.updateStatus(ToDone(url, version)).unsafeRunSync() shouldBe ()
-
-      findRow(url, version).unsafeRunSync()      shouldBe Done -> ChangeDate(now)
-      findRow(otherUrl, version).unsafeRunSync() shouldBe Sent -> changeDate
-      findRow(url, otherVersion).unsafeRunSync() shouldBe Sent -> changeDate
+        _ <- findRow(url, version).asserting(_ shouldBe Done -> ChangeDate(now))
+        _ <- findRow(otherUrl, version).asserting(_ shouldBe Sent -> changeDate)
+        _ <- findRow(url, otherVersion).asserting(_ shouldBe Sent -> changeDate)
+      } yield Succeeded
     }
   }
 
-  private lazy val url     = subscriberUrls.generateOne
-  private lazy val version = serviceVersions.generateOne
-  private lazy val message = sentences(charsGenerator = asciiPrintableChar).map(_.value).generateAs(MigrationMessage)
+  private lazy val url        = subscriberUrls.generateOne
+  private lazy val version    = serviceVersions.generateOne
+  private lazy val message    = sentences(charsGenerator = asciiPrintableChar).map(_.value).generateAs(MigrationMessage)
+  private lazy val changeDate = changeDates.generateOne
+  private lazy val now        = Instant.now().truncatedTo(MICROS)
 
-  private trait TestCase {
-    val changeDate = changeDates.generateOne
-    val now        = Instant.now().truncatedTo(MICROS)
-
-    private implicit val metricsRegistry:  TestMetricsRegistry[IO]   = TestMetricsRegistry[IO]
-    private implicit val queriesExecTimes: QueriesExecutionTimes[IO] = QueriesExecutionTimes[IO]().unsafeRunSync()
-    val currentTime = mockFunction[Instant]
-    val updater     = new StatusUpdaterImpl[IO](currentTime)
-
-    currentTime.expects().returning(now).anyNumberOfTimes()
+  private def updater(implicit cfg: DBConfig[EventLogDB]) = {
+    implicit val qet: QueriesExecutionTimes[IO] = TestQueriesExecutionTimes[IO]
+    new StatusUpdaterImpl[IO](() => now)
   }
 }
