8abbb7e6165b418b677c51b5de0a6e129bbcb227
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2024-01-02 10:56:41 +0100

    feat: JenaServer

--- triples-store-client/build.sbt
+++ triples-store-client/build.sbt
@@ -19,6 +19,16 @@
 organization := "io.renku"
 name := "triples-store-client"
 
+Test / testOptions += Tests.Setup(jenaServer("start"))
+Test / testOptions += Tests.Cleanup(jenaServer("forceStop"))
+
+def jenaServer(methodName: String): ClassLoader => Unit = classLoader => {
+  val clazz    = classLoader.loadClass("io.renku.triplesstore.client.util.TSClientJenaServer$")
+  val method   = clazz.getMethod(methodName)
+  val instance = clazz.getField("MODULE$").get(null)
+  method.invoke(instance)
+}
+
 libraryDependencies ++=
   Dependencies.jsonld4s ++
     Dependencies.luceneQueryParser ++
---
+++ triples-store-client/src/test/scala/io/renku/triplesstore/client/util/JenaServer.scala
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2024 Swiss Data Science Center (SDSC)
+ * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+ * Eidgenössische Technische Hochschule Zürich (ETHZ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.renku.triplesstore.client.util
+
+import cats.syntax.all._
+import io.renku.triplesstore.client.http.ConnectionConfig
+import org.http4s.{BasicCredentials, Uri}
+
+import scala.sys.process._
+
+class JenaServer(module: String, port: Int) {
+
+  val dbConfig: ConnectionConfig = ConnectionConfig(
+    Uri.unsafeFromString(s"http://localhost:$port"),
+    BasicCredentials(username = "admin", password = "admin").some,
+    retry = None
+  )
+
+  // When using a local Jena for development, use this env variable
+  // to not start a Jena server via docker for the tests
+  private val skipServer: Boolean = sys.env.contains("NO_JENA")
+
+  private val containerName = s"$module-test-jena"
+  private val imageVersion  = "0.0.23"
+  private val image         = s"renku/renku-jena:$imageVersion"
+  private val startCmd = s"""|docker run --rm
+                             |--name $containerName
+                             |-p $port:3030
+                             |-d $image""".stripMargin
+  private val isRunningCmd = s"docker container ls --filter 'name=$containerName'"
+  private val stopCmd      = s"docker stop -t5 $containerName"
+  private val isReadyCmd   = s"docker exec curl http://localhost:3030/$$/ping --fail"
+  private var wasRunning: Boolean = false
+
+  def start(): Unit =
+    if (skipServer) println("Not starting Jena via docker")
+    else if (checkRunning) ()
+    else {
+      println(s"Starting Jena container for module '$module' from '$image' image")
+      startCmd.!!
+      var rc = 1
+      while (rc != 0) {
+        Thread.sleep(500)
+        rc = isReadyCmd.!
+      }
+    }
+
+  private def checkRunning: Boolean = {
+    val out = isRunningCmd.lazyLines.toList
+    wasRunning = out.exists(_ contains containerName)
+    wasRunning
+  }
+
+  def stop(): Any =
+    if (!skipServer && !wasRunning) {
+      println(s"Stopping Jena container for module '$module'")
+      stopCmd.!!
+    }
+
+  def forceStop(): Any = {
+    println(s"Stopping Jena container for module '$module'")
+    stopCmd.!!
+  }
+}
