9f194bcfd7c745d7b7ce809022a372043468dd32
RenkuBot <RenkuBot@users.noreply.github.com>
2024-03-07 06:02:46 +0000

    chore: Update swagger-parser from 2.1.20 to 2.1.21

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -37,7 +37,7 @@ object Dependencies {
     val scalatestScalacheck    = "3.2.14.0"
     val sentryLogback          = "7.5.0"
     val skunk                  = "0.6.3"
-    val swaggerParser          = "2.1.20"
+    val swaggerParser          = "2.1.21"
     val widoco                 = "1.4.21"
     val wiremock               = "3.4.2"
   }
