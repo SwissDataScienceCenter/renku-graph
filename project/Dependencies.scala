f8b72092e5f82778877b0b7e337dd55300abde97
RenkuBot <RenkuBot@users.noreply.github.com>
2024-03-31 06:01:47 +0000

    chore: Update wiremock from 3.4.2 to 3.5.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,7 +39,7 @@ object Dependencies {
     val skunk                  = "0.6.3"
     val swaggerParser          = "2.1.21"
     val widoco                 = "1.4.21"
-    val wiremock               = "3.4.2"
+    val wiremock               = "3.5.2"
   }
 
   val monocle = Seq(
