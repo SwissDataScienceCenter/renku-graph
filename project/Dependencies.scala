a0df4eec25c8261869f72b127b9ea9362c66afd5
RenkuBot <53332360+RenkuBot@users.noreply.github.com>
2024-05-15 09:08:23 +0200

    chore: Update log4cats-core, log4cats-slf4j from 2.6.0 to 2.7.0 (#1906)
    Co-authored-by: RenkuBot <RenkuBot@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
     val http4sPrometheus       = "0.24.6"
     val ip4s                   = "3.5.0"
     val jsonld4s               = "0.15.0"
-    val log4cats               = "2.6.0"
+    val log4cats               = "2.7.0"
     val log4jCore              = "2.23.1"
     val logback                = "1.5.6"
     val luceneQueryParser      = "9.10.0"
