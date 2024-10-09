eede35094342df9cf4a1633631434465e49117ce
RenkuBot <RenkuBot@users.noreply.github.com>
2024-10-09 06:01:40 +0000

    chore: Update logback-classic from 1.5.8 to 1.5.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
     val jsonld4s               = "0.15.0"
     val log4cats               = "2.7.0"
     val log4jCore              = "2.24.1"
-    val logback                = "1.5.8"
+    val logback                = "1.5.9"
     val luceneQueryParser      = "9.12.0"
     val monocle                = "3.2.0"
     val owlapi                 = "5.5.0"
