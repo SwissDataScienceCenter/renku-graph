1f4997a118ff9b564d4f0138f8ff6e7661ae1dce
RenkuBot <RenkuBot@users.noreply.github.com>
2025-01-21 06:02:09 +0000

    chore: Update cats-core, cats-free from 2.12.0 to 2.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
 
   object V {
     val ammonite               = "2.4.1"
-    val catsCore               = "2.12.0"
+    val catsCore               = "2.13.0"
     val catsEffect             = "3.5.7"
     val catsEffectScalaTest    = "1.5.0"
     val catsEffectMunit        = "2.0.0"
