705379b1d000784663730163b5c62fb147a13353
RenkuBot <RenkuBot@users.noreply.github.com>
2025-04-11 06:01:35 +0000

    chore: Update lucene-analysis-common, ... from 10.0.0 to 10.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
     val log4cats               = "2.7.0"
     val log4jCore              = "2.24.2"
     val logback                = "1.5.12"
-    val luceneQueryParser      = "10.0.0"
+    val luceneQueryParser      = "10.2.0"
     val monocle                = "3.2.0"
     val owlapi                 = "5.5.0"
     val prometheus             = "0.16.0"
